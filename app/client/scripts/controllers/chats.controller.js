11644e65a31c4ec4872ccec8f8a2a5127eba3ecc
Ryan Washburn <rpwashburn@gmail.com>
2016-04-03 19:20:46 -0500

    updating styles and adding better controllers

---
+++ app/client/scripts/controllers/chats.controller.js
@@ -0,0 +1,18 @@
+angular
+  .module('Whatsapp')
+  .controller('ChatsCtrl', ChatsCtrl);
+
+function ChatsCtrl ($scope, $reactive) {
+  $reactive(this).attach($scope);
+  this.remove = remove;
+  this.helpers({
+    data() {
+      return Chats.find();
+    }
+  });
+
+  function remove (chat) {
+    this.data.splice(this.data.indexOf(chat), 1);
+  }
+
+}
--- startApp.sh
+++ startApp.sh
@@ -3,9 +3,14 @@ if [ ! -d "/meteor/app" ]; then
     echo "creating app"
     cd /meteor
     meteor create app
+    cd /meteor/app
+    ls | grep -E -v '.meteor|package.json|.gitignore' | xargs rm -rf
+    meteor remove blaze-html-templates
+    meteor remove ecmascript
+    meteor add angular
+    meteor add driftyco:ionic
 else
     echo "App already here"
     cd /meteor/app
-    meteor npm install
     meteor
 fi
